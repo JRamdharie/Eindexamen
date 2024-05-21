b1dbc7304d8cd34eb24910ca7215b9c639fd72d2
Jayden <302737774@student.rocmondriaan.nl>
2024-05-21 16:39:29 +0200

    Jayden Ramdharie US 7

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,11 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
+
 
 class ContactController extends AbstractController
 {
@@ -16,10 +17,10 @@ class ContactController extends AbstractController
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
 
-        $form = $this->createForm(ContactType::class);
 
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+
         ]);
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,26 +2,28 @@
 
 namespace App\Form;
 
-use App\Entity\Contact;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class ContactType extends AbstractType
+class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('question')
-            ->add('submit', SubmitType::class, ['label' => 'Verstuur'])
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('phoneNumber')
+            ->add('name')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Contact::class,
+            'data_class' => User::class,
         ]);
     }
 }
